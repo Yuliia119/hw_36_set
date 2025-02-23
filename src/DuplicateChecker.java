8ec4633c459eb8576831210785264db36d3f3c56
Yuliia <uliagrinenko1981@gmail.com>
2025-02-23 20:09:22 +0100

    add DuplicateChecker

---
+++ src/DuplicateChecker.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DuplicateChecker {
+    public static <T> boolean hasDuplicates (List<T> list) {
+        Set<T> uniqueElements = new HashSet<>();
+        for (T element : list) {
+            if (!uniqueElements.add(element)) {  //добавляем элемент
+                return true;  // нашёл дубликат
+            }
+        }
+        return false;   // все элементы уникальны
+    }
+}
